b77b344e54a84f74dfb4889a5d6e651c2eed1836
mai <amaomruhki@gmail.com>
2022-12-07 11:52:48 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
+
+function App() {
+	return (
+		<div className="App">
+			<AutoBatchEventHandler />
+		</div>
+	);
+}
+
+export default App;
