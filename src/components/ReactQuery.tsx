334f153ab2ab62952d98ea8bdfd42f735414e028
mai <amaomruhki@gmail.com>
2022-12-13 22:04:59 +0900

    ReactQueryでエラー中とローディング中のメッセ―ジ表示の追加

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,3 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Album = {
+	userId: number;
+	id: number;
+	title: string;
+};
+
+const fetchAlbums = async () => {
+	const result = await axios.get<Album[]>(
+		"https://jsonplaceholder.typicode.com/albums"
+	);
+	return result.data;
+};
+
 export const ReactQuery = () => {
-	return <div>ReactQuery</div>;
+	const { isLoading, error, data } = useQuery<Album[]>([""], fetchAlbums);
+
+	if (error) return <p>エラーです！</p>;
+	if (isLoading) return <p>ローディング中ですわよ</p>;
+
+	return (
+		<div>
+			<p>React Query</p>
+			{data?.map((album) => (
+				<p key={album.id}>{album.title}</p>
+			))}
+		</div>
+	);
 };
