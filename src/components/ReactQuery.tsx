f20e66d5f68bc2c5f4dfd6538322cc1c60769994
mai <amaomruhki@gmail.com>
2022-12-15 23:40:44 +0900

    Album/Todoの切り替えタブスタイル適応

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -7,21 +7,50 @@ import { TodoList } from "./TodoList";
 type Tabs = "todo" | "album";
 
 export const ReactQuery = () => {
-	const [selectedTabs, setSelectedTabs] = useState<Tabs>("todo");
+	const [selectedTab, setSelectedTab] = useState<Tabs>("todo");
+
+	const buttonStyle = {
+		padding: "12px",
+		fontSize: "16px",
+		border: "none",
+	};
+
+	const albumButtonStyle = {
+		...buttonStyle,
+		backgroundColor: selectedTab === "album" ? "royalblue" : "white",
+		color: selectedTab === "album" ? "white" : "black",
+	};
+
+	const todoButtonStyle = {
+		...buttonStyle,
+		backgroundColor: selectedTab === "todo" ? "royalblue" : "white",
+		color: selectedTab === "todo" ? "white" : "black",
+	};
+
 	const onClickTabButton = (tab: Tabs) => {
-		setSelectedTabs(tab);
+		setSelectedTab(tab);
 	};
 
 	return (
 		<div style={{ display: "flex", padding: "16px" }}>
 			<Sidebar />
 			<div style={{ flexGrow: 1 }}>
-				<button onClick={() => onClickTabButton("todo")}>TODO</button>
-				<button onClick={() => onClickTabButton("album")}>Album</button>
+				<button
+					style={todoButtonStyle}
+					onClick={() => onClickTabButton("todo")}
+				>
+					TODO
+				</button>
+				<button
+					style={albumButtonStyle}
+					onClick={() => onClickTabButton("album")}
+				>
+					Album
+				</button>
 
 				<ErrorBoundary fallback={<h1>Todo or AlbumListエラーだよ〜</h1>}>
 					<Suspense fallback={<p>Todo or AlbumListローディング中だよ〜</p>}>
-						{selectedTabs === "todo" ? <TodoList /> : <AlbumList />}
+						{selectedTab === "todo" ? <TodoList /> : <AlbumList />}
 					</Suspense>
 				</ErrorBoundary>
 			</div>
